9bcba8961549f464f950c88206ea143bbbe6bdeb
Sanghoon Lee <shpusan001@gmail.com>
2022-04-04 15:09:41 +0900

    Feat: 프론트엔드 레이아웃 구조 구축 내용: 프론트엔드 레이아웃 구조 구축 #5

---
+++ freetraveler_frontend/src/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="Home">
+        헤더
+    </div>
+  );
+}
+
+export default Header;
---
+++ freetraveler_frontend/src/layout/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <div className="Home">
+        홈페이지
+    </div>
+    <Footer/>
+    </>
+  );
+}
+
+export default Home;
