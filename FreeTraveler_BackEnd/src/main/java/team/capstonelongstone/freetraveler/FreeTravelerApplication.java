c196c6f4f9374bc1eef5c0879df501f2385c2f0e
SUNGHO PARK <qwe980813@naver.com>
2022-04-04 13:39:28 +0900

    Build : SpringBoot패키지 생성 내용 : SpringBoot 패키지 생성 및 개발환경 설정 #2

---
+++ FreeTraveler_BackEnd/src/main/java/team/capstonelongstone/freetraveler/FreeTravelerApplication.java
@@ -0,0 +1,13 @@
+package team.capstonelongstone.freetraveler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTravelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeTravelerApplication.class, args);
+    }
+
+}
