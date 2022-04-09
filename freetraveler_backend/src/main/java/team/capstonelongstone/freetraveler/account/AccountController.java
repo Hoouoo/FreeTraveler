d0a71202bfe002a618afb79aee8f0942df553038
SUNGHO PARK <qwe980813@naver.com>
2022-04-09 23:34:00 +0900

    Fix : 회원가입 기능 예외처리 추가 내용 : 회원가입 ResponseEntity  예외처리 추가 #6

---
+++ freetraveler_backend/src/main/java/team/capstonelongstone/freetraveler/account/AccountController.java
@@ -0,0 +1,25 @@
+package team.capstonelongstone.freetraveler.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import team.capstonelongstone.freetraveler.account.dto.AccountRequestDto;
+
+/**
+ * @author 박성호
+ * 회원가입 관련 Controller
+ */
+@Controller
+public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/account")
+    public ResponseEntity joinAccount(AccountRequestDto accountRequestDto) {
+        return accountService.createAccount(accountRequestDto);
+    }
+}
---
+++ freetraveler_backend/src/main/java/team/capstonelongstone/freetraveler/account/dto/AccountResponseDto.java
@@ -0,0 +1,8 @@
+package team.capstonelongstone.freetraveler.account.dto;
+
+/**
+ * @author 박성호
+ * 회원가입 관련 response Dto
+ */
+public class AccountResponseDto {
+}
