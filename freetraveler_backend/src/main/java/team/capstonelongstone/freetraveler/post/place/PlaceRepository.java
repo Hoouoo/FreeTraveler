5f1e47426fa32c544f9fef4a0406833a2cd87529
jsb12302 <jsb12302@naver.com>
2022-06-03 17:40:20 +0900

    Fix : 게시물 삭제 시 이미지 삭제 기능 추가 내용 : 게시물 삭제 시 이미지 삭제 기능 추가/#66

--- freetraveler_backend/src/main/java/team/capstonelongstone/freetraveler/post/place/PlaceRepository.java
+++ freetraveler_backend/src/main/java/team/capstonelongstone/freetraveler/post/place/PlaceRepository.java
@@ -1,6 +1,8 @@
 package team.capstonelongstone.freetraveler.post.place;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team.capstonelongstone.freetraveler.post.board.Board;
 import team.capstonelongstone.freetraveler.post.day.Day;
@@ -12,4 +14,7 @@ import java.util.List;
 public interface PlaceRepository extends JpaRepository<Place,Long> {
 
     List<Place> findAllByDay(Day dayId);
+
+    @Query("select p from Place as p where p.day.id=:id")
+    List<Place> findAllBYDayId(@Param("id")Long id);
 }
