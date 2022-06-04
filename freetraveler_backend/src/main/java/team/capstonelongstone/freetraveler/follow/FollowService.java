947e9d1f109582fc3f84e35ec9b0d2316de75247
Hoouoo <castlehoouoo98@gmail.com>
2022-06-04 22:58:00 +0900

    Fix : 팔로우 관련 기능 유지 보수 내용 : 팔로우 관련 기능 api 명세를 통한 유지 보수 #78

--- freetraveler_backend/src/main/java/team/capstonelongstone/freetraveler/follow/FollowService.java
+++ freetraveler_backend/src/main/java/team/capstonelongstone/freetraveler/follow/FollowService.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 import team.capstonelongstone.freetraveler.account.AccountRepository;
 import team.capstonelongstone.freetraveler.account.domain.Account;
 import team.capstonelongstone.freetraveler.follow.domain.Follow;
+import team.capstonelongstone.freetraveler.follow.dto.FollowResponseDto;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -39,7 +41,7 @@ public class FollowService {
 
         Account findAccount=accountRepository.findByUserId(targetId);
         if(Objects.isNull(findAccount)){ //찾는 아이디 없을때
-            return new ResponseEntity("존재하지 않는 아이디 입니다.", HttpStatus.valueOf(404));
+            return new ResponseEntity("존재하지 않는 아이디 입니다.", HttpStatus.valueOf(401));
         }
 
         if(!isPresentTargetId(account,targetId)){
@@ -52,19 +54,19 @@ public class FollowService {
         }
         else{
             System.out.println("이미 추가된 친구 입니다.");
-            return new ResponseEntity("이미 추가된 친구 입니다.",HttpStatus.BAD_REQUEST);
+            return new ResponseEntity("이미 추가되었거나 잘못된 입력값입니다.",HttpStatus.BAD_REQUEST);
         }
     }
 
     /**
      * 친구 삭제 기능
      */
-    public ResponseEntity deleteFollow(HttpServletRequest request,String targetId){
+    public ResponseEntity<?> deleteFollow(HttpServletRequest request, String targetId){
         HttpSession session=request.getSession();
         Account account = (Account) session.getAttribute("account");
 
         if(!isPresentTargetId(account,targetId)){ //존재하는 targetId가 없을때
-            return new ResponseEntity("존재하지 않는 아이디 입니다.",HttpStatus.valueOf(404));
+            return new ResponseEntity("존재하지 않는 아이디 입니다.",HttpStatus.BAD_REQUEST);
         }
         else{
             List<Follow> accountList = followRepository.findByAccount(account);
@@ -87,4 +89,14 @@ public class FollowService {
         return findByTargetId;
     }
 
+    public List<FollowResponseDto> listFollow(String targetId){
+        List<Follow> targetFollow = followRepository.listMyId(targetId);
+        List<FollowResponseDto> returnList = new ArrayList<>();
+        for (Follow follow : targetFollow) {
+            String targetUserName = accountRepository.getUserName(follow.getTargetId());
+            FollowResponseDto target = FollowResponseDto.builder().id(follow.getTargetId()).name(targetUserName).build();
+            returnList.add(target);
+        }
+        return returnList;
+    }
 }
